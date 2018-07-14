2a0eac0f9cffb5593697e21de4e57a945064079c
Nick Halvorsen <lingosalad@gmail.com>
2018-07-14 11:36:47 -0400

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div')
 
     // lodash is imported
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
+    // add the image to our existing div
+    var myIcon = new Image()
+    myIcon.src = Icon;
+    element.appendChild(myIcon)
+
+    console.log(Data)
 
     return element
 }
