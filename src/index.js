d5b3be60fcd16a1dddd9df93db2ae1ddc47796b3
Nick Halvorsen <lingosalad@gmail.com>
2018-07-15 13:15:16 -0400

    Shimming

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import _ from 'lodash'
-
 function component() {
     var element = document.createElement('div')
 
-    // lodash is imported
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = join(['Hello', 'webpack'], ' ')
+
+    this.alert("this is legacy code that is assuming 'this' is the window object, which isnt a good idea")
 
     return element
 }
 
 document.body.appendChild(component())
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        console.log('we retrieved the data. this should work in a variety of browsers')
+        console.log(json)
+    })
+    .catch(error => console.error('something went wrong when fetching this data: ', error))
