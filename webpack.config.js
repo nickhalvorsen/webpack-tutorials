843535315dafd2243b37877a0e10da15d4f7b246
Nick Halvorsen <lingosalad@gmail.com>
2018-07-15 13:58:55 -0400

    Environment Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+module.exports = env => {
+    console.log('NODE_ENV: ', env.NODE_ENV)
+    console.log('Production: ', env.production)
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
+        }
     }
 }
